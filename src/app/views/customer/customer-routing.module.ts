085bc50ce3e9e022678065cdcb219054ab0d4e21
Sergei <sergei2774551@gmail.com>
2025-10-22 10:55:16 +0200

    working site v 1.0.0

--- src/app/views/customer/customer-routing.module.ts
+++ src/app/views/customer/customer-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ForCustomerComponent} from "./for-customer/for-customer.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'for_customers', component: ForCustomerComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
